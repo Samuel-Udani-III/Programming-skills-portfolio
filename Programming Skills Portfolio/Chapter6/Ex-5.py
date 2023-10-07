632efac3bb8bf0aeebcd9882393ab19178cc4b84
Samuel-Udani-III <144774015+Samuel-Udani-III@users.noreply.github.com>
2023-10-07 23:38:43 +0400

    Add files via upload

---
+++ Programming Skills Portfolio/Chapter6/Ex-5.py
@@ -0,0 +1,31 @@
+"""
+Chapter 6
+Exercise 5
+Using the list sandwich_orders from Exercise 7-8, make sure the sandwich 'pastrami' appears in the list at least three times. Add code
+
+near the beginning of your program to print a message saying the deli has run out of pastrami, and then use a while loop to remove all
+
+occurrences of 'pastrami' from sandwich_orders. Make sure no pastrami sandwiches end up in finished_sandwiches.
+"""
+sandwich_orders = [
+    'pastrami', 'veggie', 'grilled cheese', 'pastrami',
+    'turkey', 'roast beef', 'pastrami']
+finished_sandwiches = []
+
+print("I'm sorry, we're all out of pastrami today.")
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+
+print("\n")
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    print("I'm working on your " + current_sandwich + " sandwich.")
+    finished_sandwiches.append(current_sandwich)
+
+print("\n")
+for sandwich in finished_sandwiches:
+    print("I made a " + sandwich + " sandwich.")
+    
+#Alright, this code basically has the same set-up as the previous code, except this one is about ordering pastrami sandwiches, and will let you know when pastrami is not available by printing "...we're all out of Pastrami today."
+
+#It then loops through the sandwich list once more, removing Pastrami whenever it's mentioned in the list, so it's more flexible than the code before. The second loop is unchanged, and then the third loop still prints the sandwiches, just with Pastrami being replaced by any other existing sandwich in the list.
